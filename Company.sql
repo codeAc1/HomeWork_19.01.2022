dcbfe59369059a0da16a428bb9431b1ba98626c9
Sakhavat Aliyev <sakhavatra@code.edu.az>
2022-01-19 11:32:00 +0400

    first commit

---
+++ Company.sql
@@ -0,0 +1,58 @@
+﻿Create Database Company
+
+	Use Company
+
+CREATE TABLE Departments
+	(
+		Id int primary key identity,
+		Name nvarchar(30) NOT NULL CONSTRAINT CH_NAME CHECK(LEN(Name)>2)
+	)
+
+Create Table Employees
+	(
+		ID int primary key identity,
+		FullName nvarchar(50) NOT NULL CONSTRAINT CH_FullName CHECK(LEN(FullName)>3),
+		Salary money NOT NULL CONSTRAINT CK_Salary Check(Salary>0),
+		DepartmentId int NOT NULL REFERENCES Departments(Id)
+	)
+--DepartmentID not null mehdudiyyetini gotur
+
+	Alter table Employees 
+	Alter Column DepartmentId int
+
+--Test ucun--
+--Departamentler elave et
+	Insert Into Departments
+	values ('İnformasiya texnologiyaları'),
+		   ('İnsan resursları'),
+		   ('Maliyyə və təminat'),
+		   ('Nəzarət və qiymətləndirmə')
+
+--Emekdaslar elave et
+	Insert Into Employees(FullName,DepartmentId,Salary)
+	values  ('Ruslan Muradov',6,1500),
+			('Tural Şəfiyev',6,1200),
+			('Səxavət Əliyev',3,855),
+			('Tural Seyidov',3,855),
+			('Vüsal Əsgərov',4,845),
+			('Seymur Quliyev',4,835),
+			('Elmir Quliyev',5,835),
+			('Nəcəf Məmmədov',5,550),
+			('Bəhruz Novruzov',6,550),
+			('Sadiq Əliyev',3,280),
+			('Hüseyn Məmmədov',4,280)
+
+-- Iki Cedvel arasinda munasibetleri yoxlamaq ucun sorgular
+
+--1.Emekdaslar ve Departamentler cedvelini istifade ederek FullName.Salary ve Departament adini geri qaytar.
+   --Join  üzləşdirdiyi dəyərlər null olmayanları gətirəcək
+	Select FullName,Salary,Dep.Name 'Departament Name' from Employees as Emp
+	join Departments as Dep
+	on Emp.DepartmentId=Dep.Id
+
+	--Left Join üzləşdirdiyi dəyərlər null olsa belə gətirəcək
+
+	Select FullName,Salary,Dep.Name 'Departament Name' from Employees as Emp
+	 left join Departments as Dep
+	on Emp.DepartmentId=Dep.Id
+
---
+++ Market.sql
@@ -0,0 +1,40 @@
+﻿--Market adli Database yaradin
+	Create Database Market
+	Use Market
+--Icinde Products Table-i yaradin.Product table-inda Id,Name,Price columnlari olsun
+	Create Table Products
+	(
+		Id int,
+		Name nvarchar(30),
+		Price money
+	)
+-- Products table-na yeni bir Brand columnu elave edin
+	Alter Table Products Add Brand nvarchar(15)
+
+-- Products table-a value-lar insert edin (10-15 dene product datasi kifayetdir)
+	insert into Products(Id,Name,Price,Brand)
+	values  (1,'Corek',0.65,'Bakixanov'),
+		    (2,'Günəbaxan yağı',16,'Oleos'),
+			(3,'Balqabaq yağı',42,'Oleos'),
+			(4,'Qoz yağı',35,'Oleos'),
+			(5,'Yumru düyü',7.5,'Risotto'),
+			(6,'Cay',4.3,'Beta'),
+			(7,'Simit',0.8,'AzSimit'),
+			(8,'Qend',1.5,'AzerQend'),
+			(9,'Duz',1.6,'AzerDuz'),
+			(10,'Konfet',6,'AzerKonfet')
+
+-- Qiymeti Productlarin price-larinin average-den kicik olan Products datalarinin siyahisini getiren query yazin
+	Select * from Products
+	where Price<(Select AVG(Price) from Products)
+
+-- Qiymeti 10-dan yuxari olan Product datalarinin siyahisini getiren query yazin
+	Select * from Products
+	where Price>10
+
+--Brand uzunlugu 5-den boyuk olan Productlarin siyahisini getiren query.
+--Gelen datalarda Mehsulun adi ve Brand adi 1 columnda gorsensin ve Column adi ProductInfo olsun 
+--(dersdeki name ve surname-i fullname kimi gostermeye uygun)
+	Select (Name+' '+Brand) 'ProductInfo' from Products
+	where LEN(Brand)>5
+
